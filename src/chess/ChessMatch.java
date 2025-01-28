9f3028b06379bec820658b7ee93d5e15fa2e5205
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-01-28 12:40:40 -0300

    Printing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -24,6 +24,13 @@ public class ChessMatch {
         return mat;
     }
 
+    public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+        Position position = sourcePosition.toPosition();
+        validateSourcePosition(position);
+        return board.piece(position).possibleMoves();
+
+    }
+
     public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
         Position source = sourcePosition.toPosition(); //Convertendo para posiçao da matriz
         Position target = targetPosition.toPosition(); //Convertendo para posiçao da matriz
@@ -45,6 +52,7 @@ public class ChessMatch {
         }
     }
 
+    //Valida se o movimento da posicao de origem para a de destino e possivel
     public void validateTargetPosition(Position source, Position target) {
         if (!board.piece(source).possibleMove(target)) {
             throw new ChessException("The chosen piece can't move to target position");
