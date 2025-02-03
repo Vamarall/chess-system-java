ef5835cc162dc2ba1ac6e018ac4bd8b26972f99d
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-02-03 11:16:33 -0300

    CheckMate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -51,11 +51,23 @@ public class UI {
         System.out.println();
         System.out.println("Turn: " + chessMatch.getTurn());
         System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-        if (chessMatch.getCheck()) {
-            System.out.println("CHECK!");
+        if(!chessMatch.getCheckMate()){
+            if (chessMatch.getCheck()) {
+                System.out.println("CHECK!");
+            }
+
+        }else{
+            System.out.println("CHECKMATE!");
+            System.out.println("Winner: " + chessMatch.getCurrentPlayer());
         }
+
     }
 
+    // https://stackoverflow.com/questions/2979383/java-clear-the-console
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 
     public static void printBoard(ChessPiece[][] pieces) {
 
